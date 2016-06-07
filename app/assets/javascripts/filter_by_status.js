9a8c9cac5303fab31cc440dd9bf6783c97017d18
July Tran <julyytran@gmail.com>
2016-06-06 18:45:42 -0600

    buggy read status filtering

--- app/assets/javascripts/filter_by_status.js
+++ app/assets/javascripts/filter_by_status.js
@@ -1,15 +1,19 @@
 function filterByUnread () {
   $('#filter-read').on('click', function() {
-    $.getJSON("/api/v1/links", function (data) {
-      for (var i = 0; i < data.length; i++) {
-        if (data[i].read === "false") {
-          document.getElementById(data[i].id).style.display = "none";
-        } else {
-          document.getElementById(data[i].id).style.display = "normal";
-        }
-      }
+    $.getJSON('/api/v1/links', function(data) {
+      $.each(data, function(key, link) {
+        unreadFilter(link);
+      });
     });
   });
+}
+
+function unreadFilter(link) {
+  if (link.read === "false") {
+    document.getElementById(link.id).style.display = "none";
+  } else {
+    document.getElementById(link.id).style.display = "normal";
+  }
 }
 
 function filterByRead () {
